80ee12d6e2bbeac5d22c8f1736f828391f88fb88
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-06-26 00:22:14 -0300

    Update word-to-txt.py

--- word/word-to-txt.py
+++ word/word-to-txt.py
@@ -1,77 +1,48 @@
-from openpyxl import load_workbook
+#from openpyxl import load_workbook
 #import textract
 import docx2txt
 import os
 
 # Directory for files
-file_dir = "."
+file_dir = 'C:\\Users\\apsampaio\\Documents\\Python Scripts\\word\\2019'
+#file_dir = 'C:\\Users\\Andre\\Dropbox\\Andre\\IR\\2018 ano base\\Charliana\\recibosreferenteaosatendimentosde2018' # "."
 # Caracter separador de colunas
 delimitador = "\n-----------------------------------\n"
 file_name = 'word-to-txt.txt'
 
+#filtro = {"Recebemos do (a) Sr.(a)"; "Portadora do CPF.:"; "Endereço: "; "Quantia de:"}
+filtro = ['R$', 'CPF.:', 'Data,']
+
 with open(file_name, 'w') as fileout:
 
+    total = 0
     # Get all word files
     for f in os.listdir(file_dir):
         if f.endswith(".docx"):
+            total += 1
+            filtrotemporario = set(filtro)
             path = os.path.join(file_dir, f)
             print("Parsing file: {}".format(path))
-            # Open the word
-            #text = textract.process(path)
-            # extract text
-            text = docx2txt.process(path)
             # Get file name without extension
             ff = os.path.splitext(f)[0]
+            fileout.write( ff + delimitador )
+            text = docx2txt.process(path)
+            # Filtrar
+            for line in text.splitlines():
+                Achou = False
+                for word in filtrotemporario:
+                    palavras = line.split()
+                    if word in palavras:
+                        #print('string contains a word from the word list: %s' % (word))
+                        print(line)
+                        fileout.write('\t' + line.lstrip().rstrip() + '\n' )
+                        #filtrotemporario.remove(word)
+                        Achou = True
+                #if Achou:
+                #    filtrotemporario.remove(word)
             # write to file
-            fileout.write( ff + delimitador + str(text) + delimitador )
-            '''
-            book = load_workbook(path, read_only=True)
-            print("Workbook loaded")
-            sheets = book.sheetnames
-            print(str(len(sheets)) + " Sheets da planilhas: " + str(sheets))
-            '''
-            # Get file name without extension
-            #ff = os.path.splitext(f)[0]
-            # Output file generated
-            #file_name = "{0}.txt".format(ff)
+            #fileout.write( ff + delimitador + str(text) + delimitador )
+            fileout.write( delimitador )
             print("Writed to file: {}".format(file_name))
-            #nlp_file = codecs.open(file_name, 'w', encoding='utf-8')
-            '''
-            with open(file_name, 'w') as fileout:
-                # Iterate through worksheets and get values
-                for sheet in book:
-                    print("Processando aba {0} ".format(sheet))
-                    #print("Processando aba {0} com Dimensões = {1}".format(sheet, sheet.dimensions))
-                    #print("Minimum row: {0}".format(sheet.min_row))
-                    #print("Maximum row: {0}".format(sheet.max_row))
-                    #print("Minimum column: {0}".format(sheet.min_column))
-                    #print("Maximum column: {0}".format(sheet.max_column))
-                    
-                    # Get the  values : nao escreve celulas vazias
-                    for linha in range(1, sheet.max_row+1):
-                        fileout.write( "\n")
-                        for coluna in range(1, sheet.max_column+1):
-                            cell = sheet.cell(column=coluna, row=linha).value
-                            if cell is not None:
-                                fileout.write( str(cell) + delimitador )
-                        #Escrevendo a ultima coluna e quebrando a linha
-                        #cell = sheet.cell(column=sheet.max_column, row=linha).value
-                        #if cell is not None:
-                            #fileout.write( str(cell) + "\n")
-                    header = sheet.row(0)
-                    for rx in range(1, sheet.nrows):
-                        for cx in range(0, sheet.ncols):
-                            headerVal = convtostr(header[cx].value, 0)
-                            if 'date' in headerVal.lower():
-                                cellVal = convtostr(sheet.cell_value(rx, cx), 1)
-                            else:
-                                cellVal = convtostr(sheet.cell_value(rx, cx), 0)
-                            if cellVal !="":
-                                nlp_text += "{0} is {1} and ".format(headerVal, cellVal)
-                                nlp = nlp_text.replace('\n', ' ')
-                        nlp += "\n"
-            '''
-
+    print(delimitador + "\nTotal de arquivos analisados = " + str(total))
 fileout.close()
-
-#book.save('dimensions.xlsx')
