417da46db8b06f8aacad2efe22ffb1c2ff91b505
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-06-26 22:59:06 -0300

    Update word-to-txt.py

--- word/word-to-txt.py
+++ word/word-to-txt.py
@@ -16,7 +16,7 @@ ANO = '2019'
 file_name = CPFDRA+'-'+ANO+'.txt'
 
 #filtro = {"Recebemos do (a) Sr.(a)"; "Portadora do CPF.:"; "Endereço: "; "Quantia de:"}
-filtro = ['Data,', 'R$', 'do CPF.:', 'Recebemos']
+filtro = ['Data,', 'R$', 'CPF.:', 'Recebemos']
 campos_com_base_nos_filtros = {}
 # campos = {'data': '05/01/'+ANO, 'conta': '', 'valor': '0,00', 'CPFPAGOU': '', 'CPFUSOU': '', 'Historico': ''}
 # data = DD/MM/AAAA ou DD/MM/AA
@@ -25,72 +25,84 @@ campos_com_base_nos_filtros = {}
 # CPF da Dra
 # Historico = Campo texto com ate 250 chars = usar Nome do Paciente ; GOOGLE ADS ; ALUGUEL
 
-with open(file_name, 'w') as fileout:
-    # logs
-    with open('log-'+file_name, 'w') as logfileout:
-        # criar os gastos mensais com propaganda e aluguel
-        for mes in range(1, 13):
-            campos = {'data': '05/01/'+ANO, 'conta': '', 'valor': '0,00', 'CPFPAGOU': '', 'CPFUSOU': '', 'Historico': ''}
-            for conta in ['4001', '4017']:
-                campos['conta'] = conta
-                if mes < 10:
-                    campos['data'] = '05/0'+str(mes)+'/'+ANO
-                else:
-                    campos['data'] = '05/'+str(mes)+'/'+ANO
-                if conta == '4001':
-                    campos['valor'] ='1000'
-                    campos['Historico'] ='GASTO COM ALUGUEL'
-                else : #  conta == '4017':
-                    campos['valor'] ='350'
-                    campos['Historico'] =' GASTO COM GOOGLE ADS'
-                campos['CPFPAGOU'] = CPFDRA
-                # campos para gravar no arquivo
-                fileout.write( campos['data']+';'+campos['conta']+';'+campos['valor']+';'+campos['CPFPAGOU']+';'+campos['CPFUSOU']+';'+campos['Historico']+'\n' )
-        # criar os recebimentos dos pacientes
-        total = 0
-        erro = 0
-        # Get all word files
-        for f in os.listdir(file_dir):
-            campos = {'data': '05/01/'+ANO, 'conta': '', 'valor': '0,00', 'CPFPAGOU': '', 'CPFUSOU': '', 'Historico': ''}
-            if f.endswith(".docx"):
-                total += 1
-                path = os.path.join(file_dir, f)
-                print("Parsing file: {}".format(path))
-                # Get file name without extension
-                ff = os.path.splitext(f)[0]
-                logfileout.write( ff + delimitador )
-                text = docx2txt.process(path)
-                # Filtrar
-                for line in text.splitlines():
-                    for word in filtro:
-                        if word in line: #palavras:
-                            #print('string contains a word from the word list: %s' % (word))
-                            #print(line)
-                            logfileout.write('\t' + line.lstrip().rstrip() + '\n' )
-                            campos_com_base_nos_filtros[word]=line.lstrip().rstrip()
-                # write to file
-                # escrevendo os campos
-                # pega os valores do dict: campos_com_base_nos_filtros[word]
-                campos['data'] = campos_com_base_nos_filtros[filtro[0]].replace('Data, ','').replace('.','') # filtro = 'Data,'
-                campos['conta'] = '1000'
-                campos['valor'] = campos_com_base_nos_filtros[filtro[1]].replace('R$ ','').replace('.','') # filtro = 'R$'
-                if filtro[2] in campos_com_base_nos_filtros.keys(): # verificar se consta o CPF do paciente
-                    patternCPF = r"\d{3}\.?\d{3}\.?\d{3}.?\d{2}"
-                    texto = campos_com_base_nos_filtros[filtro[2]] # filtro = 'do CPF.:'
-                    campos['CPFPAGOU'] = str(re.findall(patternCPF, texto)[0]).replace('.','').replace('-','')
-                    campos['CPFUSOU'] = campos['CPFPAGOU']
-                else: # nao achou CPF do paciente
-                    campos['CPF'] = ''
-                    print('\tERRO COM O CPF DO PACIENTE - NAO ENCONTRADO')
-                    logfileout.write('\tERRO COM O CPF DO PACIENTE - NAO ENCONTRADO\n')
-                    erro += 1
-                campos['Historico'] = 'Arquivo fonte: ' + ff # info repetida + ' : ' + campos_com_base_nos_filtros['Recebemos']
-                # campos para gravar no arquivo
-                fileout.write( campos['data']+';'+campos['conta']+';'+campos['valor']+';'+campos['CPFPAGOU']+';'+campos['CPFUSOU']+';'+campos['Historico']+'\n' )
+def getCPF(texto):
+    patternCPF = r"\d{3}\.?\d{3}\.?\d{3}.?\d{2}"
+    listCPF = re.findall(patternCPF, texto)
+    if listCPF:
+        return str(listCPF[0]).replace('.','').replace('-','')
+    else:
+        return ''
 
-                #print("Escrito no arquivo: {}".format(file_name))
-                            #fileout.write( ff + delimitador + str(text) + delimitador )
-                logfileout.write( delimitador )
-        print(delimitador + "\nTotal de arquivos analisados = " + str(total) + '\n Total com ERROS (sem CPF) = ' + str(erro))
-    logfileout.close()
-fileout.close()
+if __name__ == "__main__":
+    with open(file_name, 'w') as fileout:
+        # logs
+        with open('log-'+file_name, 'w') as logfileout:
+            # criar os gastos mensais com propaganda e aluguel
+            for mes in range(1, 13):
+                campos = {'data': '05/01/'+ANO, 'conta': '', 'valor': '0,00', 'CPFPAGOU': '', 'CPFUSOU': '', 'Historico': ''}
+                for conta in ['4001', '4017']:
+                    campos['conta'] = conta
+                    if mes < 10:
+                        campos['data'] = '05/0'+str(mes)+'/'+ANO
+                    else:
+                        campos['data'] = '05/'+str(mes)+'/'+ANO
+                    if conta == '4001':
+                        campos['valor'] ='1000'
+                        campos['Historico'] ='GASTO COM ALUGUEL'
+                    else : #  conta == '4017':
+                        campos['valor'] ='350'
+                        campos['Historico'] =' GASTO COM GOOGLE ADS'
+                    campos['CPFPAGOU'] = CPFDRA
+                    # campos para gravar no arquivo
+                    fileout.write( campos['data']+';'+campos['conta']+';'+campos['valor']+';'+campos['CPFPAGOU']+';'+campos['CPFUSOU']+';'+campos['Historico']+'\n' )
+            # criar os recebimentos dos pacientes
+            total = 0
+            erro = 0
+            # Get all word files
+            for f in os.listdir(file_dir):
+                campos = {'data': '05/01/'+ANO, 'conta': '', 'valor': '0,00', 'CPFPAGOU': '', 'CPFUSOU': '', 'Historico': ''}
+                if f.endswith(".docx"):
+                    total += 1
+                    path = os.path.join(file_dir, f)
+                    print("Parsing file: {}".format(path))
+                    # Get file name without extension
+                    ff = os.path.splitext(f)[0]
+                    logfileout.write( ff + delimitador )
+                    text = docx2txt.process(path)
+                    # Filtrar
+                    for line in text.splitlines():
+                        for word in filtro:
+                            if word in line: #palavras:
+                                #print('string contains a word from the word list: %s' % (word))
+                                #print(line)
+                                if getCPF(line) == getCPF(CPFDRA): # pular o CPFDRA
+                                    continue
+                                logfileout.write('\t' + line.lstrip().rstrip() + '\n' )
+                                campos_com_base_nos_filtros[word]=line.lstrip().rstrip()
+                    # write to file
+                    # escrevendo os campos
+                    # pega os valores do dict: campos_com_base_nos_filtros[word]
+                    campos['data'] = campos_com_base_nos_filtros[filtro[0]].replace('Data, ','').replace('.','') # filtro = 'Data,'
+                    campos['conta'] = '1000'
+                    campos['valor'] = campos_com_base_nos_filtros[filtro[1]].replace('R$ ','').replace('.','') # filtro = 'R$'
+                    if filtro[2] in campos_com_base_nos_filtros.keys(): # verificar se consta o CPF do paciente
+                        texto = campos_com_base_nos_filtros[filtro[2]] # filtro = 'CPF.:'
+                        if getCPF(texto) == getCPF(CPFDRA): # pular o CPFDRA
+                            continue
+                        campos['CPFPAGOU'] = getCPF(texto)
+                        campos['CPFUSOU'] = campos['CPFPAGOU']
+                    else: # nao achou CPF do paciente
+                        campos['CPF'] = ''
+                        print('\tERRO COM O CPF DO PACIENTE - NAO ENCONTRADO')
+                        logfileout.write('\tERRO COM O CPF DO PACIENTE - NAO ENCONTRADO\n')
+                        erro += 1
+                    campos['Historico'] = 'Arquivo fonte: ' + ff # info repetida + ' : ' + campos_com_base_nos_filtros['Recebemos']
+                    # campos para gravar no arquivo
+                    fileout.write( campos['data']+';'+campos['conta']+';'+campos['valor']+';'+campos['CPFPAGOU']+';'+campos['CPFUSOU']+';'+campos['Historico']+'\n' )
+
+                    #print("Escrito no arquivo: {}".format(file_name))
+                                #fileout.write( ff + delimitador + str(text) + delimitador )
+                    logfileout.write( delimitador )
+            print(delimitador + "\nTotal de arquivos analisados = " + str(total) + '\n Total com ERROS (sem CPF) = ' + str(erro))
+        logfileout.close()
+    fileout.close()
